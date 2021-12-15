4d4f3cd13aca138e829ad5b601d8cdb6a971fc78
slubowsky <270589+slubowsky@users.noreply.github.com>
2021-12-15 10:51:01 -0500

    quiz comments

--- JavaScript/HW83/test.js
+++ JavaScript/HW83/test.js
@@ -2,6 +2,7 @@
     'use strict';
 
     const request = await fetch('test.json');
+    // SL - theItems? Arent these the orders?
     const theItems = await request.json();
     console.log(theItems)
 
@@ -15,17 +16,19 @@
         }
     }
 
+    // SL - what is this new Item() used for? Why is it created?
     let theItem = new Item();
     for (let i = 0; i < theItems.length; i++) {
         theItems[i].items.forEach(element => {
 
             element.total = element.total / element.quantity;
 
+            // SL - you create an Item instance for each item, great, but your not storing these in the orders. You just immediately append the data to the dom and throw them away...
             theItem = new Item(element.item, element.total, element.quantity);
 
             console.log(theItem)
             theCustomer
-            
+
                 .append(`<div>Items: </div>${element.item}`)
                 .append(`<div>Quantity: </div>${element.quantity}`)
                 .append(`<div>Price per unit: </div>${element.total}`)
@@ -41,6 +44,7 @@
             this.itemsArray = itemsArray;
         }
 
+        // SL - ?
         get theTotalCost() {
 
             return 'the sum of, the product of the Quanity and price per unit, of all orders';
@@ -49,12 +53,13 @@
 
     }
 
-  
- 
+
+
 
     theItems.forEach(element => {
+      // SL - as above these ar enot going to be Item instances they are the raw item objects we got from JSON.
       let  theOrders = new Order(element.customer, element.address, element.items)
-           
+
         console.log(theOrders)
 
         theCustomer.append(`<div>Customer: </div>${element.customer}`)
@@ -69,6 +74,7 @@
 
     });
 
+    // SL - nice start but looks like you need more practice...
 
 
 
